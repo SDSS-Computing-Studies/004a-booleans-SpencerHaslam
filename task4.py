3a6ab5dc77dde5088b6dc7f8e6211a0269323ef0
SpencerHaslam <71406886+SpencerHaslam@users.noreply.github.com>
2020-09-29 12:19:27 -0700

    Update task4.py

--- task4.py
+++ task4.py
@@ -9,3 +9,23 @@
 # Outputs:
 # "the sentence contains password"
 # "the sentence does not contain password"
+
+
+#Strings
+#*needle* in *haystack*
+#"i" in "team" is evaluated as False
+#"fun" in "funeral" is evaluated as True
+
+#if "fun" in "funeral":
+#    print("there is fun in funeral")
+
+#if 3 in range(1,10):
+
+
+num = input("Enter a sentence")
+if "password" in num:
+    print("the sentence contains password")
+
+
+else:
+    print("the sentence does not contain password")
