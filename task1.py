1dd8b4b03b0d20fb78dac865993bc0e7bcd1e6c8
SpencerHaslam <71406886+SpencerHaslam@users.noreply.github.com>
2020-09-29 11:38:09 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -11,3 +11,10 @@
 # "The number is larger than 100"
 # "The number is smaller than 100"
 # "The number is 100"
+num = input("Enter a number")
+nom = float(num)
+if nom < 100:
+    print("The number is smaller than 100")
+
+else:
+    print("The number is larger than 100")
