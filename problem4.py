9be88124cdcfaff049a85bf36761ed76b7f4469c
SpencerHaslam <71406886+SpencerHaslam@users.noreply.github.com>
2020-09-30 13:10:20 -0700

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -12,3 +12,14 @@
 # - "that is a right triangle"
 # - "that is an acute triangle"
 # - "that is an obtuse triangle"
+import math
+a = input("Enter a number")
+b = input("Enter a second number")
+c = input("Enter a third number")
+A = float(a)
+B = float(b)
+C = float(c)
+h = max(A, B, C)
+s = min (A, B, C)
+m = (A + B + C) - (h + s)
+#   x**2 + y**2 = h**2
