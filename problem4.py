1893961345260dd7f0d99a8d81c8f20b54294647
SpencerHaslam <71406886+SpencerHaslam@users.noreply.github.com>
2020-10-01 12:56:27 -0700

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -1,13 +1,3 @@
-#! python3
-# Have the user enter in 3 numerical values, representing the side lengths of a triangle. 
-# Determine if the values are close enough to make a right triangle. 
-# It is close enough if the expected length of the hypotenuse and the actual length 
-# differ by no more than 2% 
-# (2 marks)
-
-# Inputs:
-# - 3 numbers, in any order
-
 # Outputs:
 # - "that is a right triangle"
 # - "that is an acute triangle"
@@ -19,17 +9,17 @@ c = input("Enter a third number")
 A = float(a)
 B = float(b)
 C = float(c)
-h = max(A, B, C)
-s = min (A, B, C)
-m = (A + B + C) - (h + s)
+uHyp = max(A, B, C)
+minVal = min (A, B, C)
+midVal = (A + B + C) - (uHyp + minVal)
 #   x**2 + y**2 = h**2
-p = (s**2) + (m**2)
-k = math.sqrt(p)
-j = h * 1.02
-o = j - h
-if k > o:
+p = (minVal**2) + (midVal**2)
+cHyp = math.sqrt(p)
+upperLimit = cHyp * 1.02
+lowerLimit = cHyp * 0.98
+if uHyp > upperLimit:
     print("that is an obtuse triangle")
-elif k < o:
+elif uHyp < lowerLimit:
     print("that is an acute triangle")
-elif k == o:
+else:
     print("that is a right triangle")
